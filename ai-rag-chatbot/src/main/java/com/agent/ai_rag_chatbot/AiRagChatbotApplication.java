850ed2f0dbfda41c0998f64f29c2fbad08a1c2b5
rajeshpakkala <rajeshpakkala786@gmail.com>
2026-08-16 19:53:18 +0530

    first commit

---
+++ ai-rag-chatbot/src/main/java/com/agent/ai_rag_chatbot/AiRagChatbotApplication.java
@@ -0,0 +1,13 @@
+package com.agent.ai_rag_chatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiRagChatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiRagChatbotApplication.class, args);
+	}
+
+}
